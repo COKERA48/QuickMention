74e095013bce68e91e6b20f21c250a881993af2b
Ashley Coker <cokera48@lsus.edu>
2018-02-01 11:43:07 -0600

    added temporary sign out button

--- app/src/main/java/com/testingtestingtesting/ashley/quickmentiontest/MainActivity.java
+++ app/src/main/java/com/testingtestingtesting/ashley/quickmentiontest/MainActivity.java
@@ -1,13 +1,17 @@
 package com.testingtestingtesting.ashley.quickmentiontest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button buttonNewTask;
+    private Button buttonSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +20,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonNewTask = (Button) findViewById(R.id.buttonNewTask);
         buttonNewTask.setOnClickListener(this);
+        buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
+        buttonSignOut.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+
+        if (v == buttonSignOut){
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+        }
 
     }
 }
