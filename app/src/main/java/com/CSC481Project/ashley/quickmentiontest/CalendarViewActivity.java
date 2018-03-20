5ab81e40729aa6e7f63dc327d04eb5da6ee0009f
blane-defriend <33462795+blane-defriend@users.noreply.github.com>
2018-03-19 22:45:16 -0500

    added task list under calendar, haven't gotten filtering working yet

--- app/src/main/java/com/CSC481Project/ashley/quickmentiontest/CalendarViewActivity.java
+++ app/src/main/java/com/CSC481Project/ashley/quickmentiontest/CalendarViewActivity.java
@@ -1,14 +1,29 @@
 package com.CSC481Project.ashley.quickmentiontest;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarViewActivity extends AppCompatActivity {
 
     private CalendarView calendarView;
+    private ListView listview;
     private TextView textbox;
+    private DatabaseHelper dbHelper;
+    private static final String TAG = "CalendarViewActivity";
+    private String selectedDate;
+    private SimpleDateFormat dateFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +35,28 @@ public class CalendarViewActivity extends AppCompatActivity {
         //calendarView is already set to current date on creation
         //calendarView.setDate(new Date().getTime()); //set view to current date
 
+        listview = (ListView) findViewById(R.id.calendarListView);
         textbox = (TextView) findViewById(R.id.calendarViewTextbox);
+        dbHelper = new DatabaseHelper(this);
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        populateListView();
+
+    }
+
+    private void populateListView() {
+        Log.d(TAG, "populateListView: Displaying data to ListView");
+
+        Cursor data = dbHelper.getTasks();
+
+        ArrayList<String> listData = new ArrayList<>();
+        while(data.moveToNext()) {
+            textbox.setText(selectedDate + " " + data.getString(2));
+            listData.add(data.getString(1) + "\t\t\t" + data.getString(2) + "\t\t\t" + data.getString(3) + "\t\t\t" + data.getString(5));
+
+        }
+
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+        listview.setAdapter(adapter);
     }
 
     //listener is called whenever a new date is picked
@@ -34,7 +70,9 @@ public class CalendarViewActivity extends AppCompatActivity {
         @Override
         public void onSelectedDayChange(CalendarView view, int year, int month, int day)
         {
-            textbox.setText(month + "/" + day + "/" + year);
+
+            selectedDate = (month+1) + "/" + day + "/" + year;
+            populateListView();
         }
     }
 }
