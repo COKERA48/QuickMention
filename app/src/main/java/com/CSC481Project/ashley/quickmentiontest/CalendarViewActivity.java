0d2835e94e22e036d35b9b533b2a2b0a323d1f65
blane-defriend <33462795+blane-defriend@users.noreply.github.com>
2018-04-05 12:07:25 -0500

    added loader to calendarView

--- app/src/main/java/com/CSC481Project/ashley/quickmentiontest/CalendarViewActivity.java
+++ app/src/main/java/com/CSC481Project/ashley/quickmentiontest/CalendarViewActivity.java
@@ -1,7 +1,12 @@
 package com.CSC481Project.ashley.quickmentiontest;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -10,17 +15,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
+import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
-public class CalendarViewActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class CalendarViewActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, NavigationView.OnNavigationItemSelectedListener {
 
     private CalendarView calendarView;
     private NavigationView menu;
@@ -32,7 +43,9 @@ public class CalendarViewActivity extends AppCompatActivity implements Navigatio
     private String selectedDate;
     private SimpleDateFormat dateFormat;
     private DecimalFormat digitFormatter;
+    SimpleCursorAdapter adapter;
 
+    private static final int VEHICLE_LOADER = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +75,32 @@ public class CalendarViewActivity extends AppCompatActivity implements Navigatio
         //commenting out populateListView because it uses old dbhelper and not QMContent
         //populateListView();
 
+        adapter = new SimpleCursorAdapter(this,
+                R.layout.single_row_task,
+                null,
+                new String[] { QMContract.TaskEntry.KEY_NAME, QMContract.TaskEntry.KEY_START_DATE, QMContract.TaskEntry.KEY_START_TIME },
+                new int[] { R.id.textViewTaskName, R.id.textViewTaskDate, R.id.textViewTaskTime }, 0);
+
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Intent intent = new Intent(CalendarViewActivity.this, CreateTaskActivity.class);
+
+                Uri currentVehicleUri = ContentUris.withAppendedId(QMContract.TaskEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentVehicleUri);
+
+
+                startActivity(intent);
+
+            }
+        });
+
+        getLoaderManager().initLoader(VEHICLE_LOADER, null, this);
     }
 
     private void populateListView() {
@@ -95,7 +134,50 @@ public class CalendarViewActivity extends AppCompatActivity implements Navigatio
             Log.d(TAG, selectedDate);
             //commenting out populateListView because it uses dbhelper and not QMContent
             //populateListView();
+
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String[] projection = {
+                QMContract.TaskEntry._ID1,
+                QMContract.TaskEntry.KEY_NAME,
+                QMContract.TaskEntry.KEY_START_DATE,
+                QMContract.TaskEntry.KEY_START_TIME,
+                QMContract.TaskEntry.KEY_END_DATE,
+                QMContract.TaskEntry.KEY_END_TIME,
+                QMContract.TaskEntry.KEY_REPEATS,
+                QMContract.TaskEntry.KEY_NOTES
+
+        };
+
+        Calendar c = Calendar.getInstance();
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+        String selectDate = dateFormat.format(c.getTime());
+        String selection = "(" + QMContract.TaskEntry.KEY_START_DATE + " = '" + selectedDate + "')";
+
+
+        return new CursorLoader(this,   // Parent activity context
+                QMContract.TaskEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                selection,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        adapter.swapCursor(cursor);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
+
     }
 
     //from menu interface
