d483a5023b11ff2aad3ffcbe03a4ffa5cc3b4006
blane-defriend <33462795+blane-defriend@users.noreply.github.com>
2018-04-16 23:30:26 -0500

    set title on view tasks screen

--- app/src/main/java/com/CSC481Project/ashley/quickmentiontest/DisplayTasksActivity.java
+++ app/src/main/java/com/CSC481Project/ashley/quickmentiontest/DisplayTasksActivity.java
@@ -33,6 +33,7 @@ public class DisplayTasksActivity extends AppCompatActivity implements LoaderMan
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_tasks);
+        setTitle("All Tasks");
 
         //setup side menu, toggle button, and toolbar
         NavigationView menu = findViewById(R.id.navigationView);
